fe36e1a474cd44347f2975167f71a0cfed1ddfe7
SOHAMBIDS <sohambidwai07@gmail.com>
2024-05-19 13:35:01 +0530

    Committed on 19 May 2024, Worked On User Login Using Spring Security

---
+++ src/main/java/com/smartcontactmanager/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package com.smartcontactmanager.dao;
+
+import com.smartcontactmanager.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    //in below line email value come as dynamic value
+    @Query("select u from User u where u.email = email")
+    public User getUserByEmail(@Param("email") String email);
+
+}
